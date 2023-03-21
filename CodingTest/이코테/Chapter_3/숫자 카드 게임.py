4b3e9a9d300936d0d72fee99a2dd1b75996c4c49
hantae <hantae96@naver.com>
2023-03-21 10:46:41 +0900

    상하좌우

---
+++ CodingTest/이코테/Chapter_3/숫자 카드 게임.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.stdin = open("CodingTest/이코테/Chapter_3/input.txt")
+n,m = map(int,input().split())
+
+result = 0
+for i in range(n):
+    data  = list(map(int,input().split()))
+    min_value = min(data)
+    result = max(result,min_value)
+
+print(result)
---
+++ CodingTest/이코테/Chapter_4/상하좌우.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.stdin = open("CodingTest/이코테/Chapter_3/input.txt")
+n = int(input())
+control = list(input().split())
+
+control_map = {"L":[0,-1],"R":[0,1],"U":[-1,0],"D":[1,0]}
+
+now = [0,0]
+
+for idx,move in enumerate(control):
+    next = [i+j for i,j in zip(now,control_map.get(move))]
+    if (-1<next[0]<n) and (-1<next[1]<n):
+        now = next
+now = [i+1 for i in now]
+print(now)  
