e9ee3c20bba3ab9519de0d9a6e98212d2da28efd
hantae <85547680+hantae96@users.noreply.github.com>
2023-03-25 13:08:32 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,20 @@
+last_idx = 0
+
+def check(cook):
+    if cook[-4:] == [1,2,3,1]:
+        for _ in range(4):
+            cook.pop()
+        return True
+    return False
+
+def solution(ingredient):
+    answer = 0
+    cook = []
+    # 빵이 들어왔는데 완성이 안되면 다 버린다.
+
+    for i in ingredient:
+        cook.append(i)
+        if check(cook):
+            answer+=1
+            
+    return answer
