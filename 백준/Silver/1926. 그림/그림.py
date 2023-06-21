4c7bc90fdebb1a120cf9f1b6fae4eeba0657923c
hantae <85547680+hantae96@users.noreply.github.com>
2023-06-21 10:12:52 +0900

    [Silver I] Title: 그림, Time: 336 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,48 +1,46 @@
 import sys
 from collections import deque
 
+
+def bfs(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    dq = deque()
+
+    matrix[x][y] = 0
+    dq.append((x, y))
+    cnt = 1
+
+    while dq:
+        (x, y) = dq.popleft()
+        for i in range(4):
+            n_x = x + dx[i]
+            n_y = y + dy[i]
+            if (-1 < n_x < n) and (-1 < n_y < m):
+                if matrix[n_x][n_y] == 1:
+                    matrix[n_x][n_y] = 0
+                    dq.append((n_x, n_y))
+                    cnt += 1
+
+    return cnt
+
+
 #sys.stdin = open("input.txt")
-
-
 n, m = map(int, input().split())
+matrix = [list(map(int, input().split())) for i in range(n)]
 
-martix = []
-check = [[0] * m for i in range(n)]
-
-for i in range(n):
-    martix.append(list(map(int, input().split())))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-dq = deque()
-res = -100000
-count = 0
+cnt = 0
+size = []
 for i in range(n):
     for j in range(m):
-        if martix[i][j] == 1:
-            count += 1
-            start = [i, j]
-            martix[i][j] = 0
-            dq.append(start)
-            tmp = 1
+        if matrix[i][j] == 1:
+            cnt += 1
+            size.append(bfs(i, j))
 
-            while dq:
-                now = dq.popleft()
-                for k in range(4):
-                    next_x = now[0] + dx[k]
-                    next_y = now[1] + dy[k]
-                    if next_x < 0 or next_x >= n or next_y < 0 or next_y >= m:
-                        continue
-                    if martix[next_x][next_y] == 1:
-                        tmp += 1
-                        martix[next_x][next_y] = 0
-                        dq.append([next_x, next_y])
-            if tmp > res:
-                res = tmp
 
-if count != 0:
-    print(count)
-    print(res)
+if len(size) != 0:
+    print(cnt)
+    print(max(size))
 else:
     print(0)
     print(0)
