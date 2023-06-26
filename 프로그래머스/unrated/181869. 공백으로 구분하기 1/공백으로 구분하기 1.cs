27e22c48756234b0d54ff178feb5cf756ff44e9f
hantae <85547680+hantae96@users.noreply.github.com>
2023-06-26 11:39:49 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.30 ms, Memory: 30.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public string[] solution(string my_string) {
+        string[] answer = new string[] {};
+        answer=my_string.Split(' ');
+    
+        return answer;
+    }
+}
