43c721baf0f3451ec278fb31bdba9253147b67d0
hantae <85547680+hantae96@users.noreply.github.com>
2024-01-10 08:05:31 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,39 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-pair<int,int> solve(vector<int> v){
-    pair<int,int> p;
-    int sum = accumulate(v.begin(),v.end(),0);
-    for(int i = 0;i<8;i++){
-        for (int j = i+1;j<9;j++){
-            if(sum - v[i] - v[j] == 100){
-                p.first = v[i];
-                p.second = v[j];
-                return p;
+int main() {
+    vector<int> answer;
+    int sum = 0;
+
+    for (int i = 0; i < 9; i++) {
+        int num;
+        cin >> num;
+        answer.push_back(num);
+        sum += num;
+    }
+
+    pair<int, int> find;
+    for (int i = 0; i < 9; i++) {
+        for (int j = i + 1; j < 9; j++) {
+            if (sum - answer[i] - answer[j] == 100) {
+                find = {answer[i], answer[j]};
+                break;
             }
         }
     }
-}
-
-int main(){
-    vector<int> v;
-    int num;
-    for(int i = 0;i<9;i++){
-        cin >> num;
-        v.push_back(num);
-    }
-
-    vector<int> answer;
-    pair<int,int> p = solve(v);
-
-    for(int i = 0;i<9;i++){
-        if(v[i] != p.first && v[i] != p.second){
-            answer.push_back(v[i]);
-        }
-    }
-
     sort(answer.begin(),answer.end());
-    for (int i = 0;i<answer.size();i++){
+
+    for (int i = 0; i < 9; i++) {
+        if (answer[i] == find.first || answer[i] == find.second) {
+            continue;
+        }
         cout << answer[i] << endl;
-    }    
+    }
+
+    return 0;
 }
