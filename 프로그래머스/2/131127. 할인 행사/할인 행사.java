a356bce515cc8b25750ca165788534d94b71c2c5
hantae <85547680+hantae96@users.noreply.github.com>
2024-09-09 20:07:30 +0900

    [level 2] Title: 할인 행사, Time: 69.33 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,44 +3,37 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int limit = 10;
-
-        // 원하는 상품과 그 수량을 맵에 저장
-        HashMap<String, Integer> wantMap = new HashMap<>();
-        for (int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
+        for(int i = 0;i<want.length;i++){
+            String key = want[i];
+            Integer value = number[i];
+            map.put(key,value);                        
         }
-
-        // 슬라이딩 윈도우 방식으로 10일 동안의 할인 항목을 확인
-        for (int i = 0; i <= discount.length - limit; i++) {
-            // 매번 새로운 복사본을 사용
-            HashMap<String, Integer> tempMap = new HashMap<>(wantMap);
-            boolean isValid = true;
-
-            // 10일 동안의 할인 항목을 확인
-            for (int j = i; j < i + limit; j++) {
-                String elem = discount[j];
-                
-                if (!tempMap.containsKey(elem)) {
-                    isValid = false;
+        
+        
+        int startRange = discount.length - 9;
+        for(int i = 0;i<startRange;i++){
+            HashMap<String,Integer> temp = new HashMap<String,Integer>(map);
+            boolean check = true;
+            for(int j = i;j<i+10;j++){
+                String key = discount[j];
+                if(!temp.containsKey(key)){
+                    check = false;
                     break;
                 }
-
-                int value = tempMap.get(elem);
-                if (value == 0) {
-                    isValid = false;
+                int value = temp.get(key);
+                if(value == 0){
+                    check = false;
                     break;
-                } else {
-                    tempMap.put(elem, value - 1);
                 }
+                temp.put(key,--value);
             }
-
-            // 모든 조건을 만족하는 경우에만 answer 증가
-            if (isValid) {
+            
+            if(check){
                 answer++;
             }
         }
-
+        
         return answer;
     }
 }
