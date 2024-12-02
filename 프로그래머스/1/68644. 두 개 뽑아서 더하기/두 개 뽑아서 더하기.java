59948701fbb67086eff4594ebf0646fe1dec81a7
hantae <85547680+hantae96@users.noreply.github.com>
2024-12-02 22:41:41 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 7.13 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,28 +1,22 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        // HashSet을 사용하여 중복된 합을 제거
-        HashSet<Integer> set = new HashSet<>();
+        int[] answer = {};
         
-        // 모든 쌍의 합을 HashSet에 추가
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                set.add(numbers[i] + numbers[j]);
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0;i<numbers.length;i++){
+            for(int j = i+1;j<numbers.length;j++){
+                int num = numbers[i] + numbers[j];
+                
+                if(!list.contains(num)){
+                    list.add(num);
+                }
             }
         }
-        
-        // HashSet을 List로 변환하고 정렬
-        List<Integer> temp = set.stream()
-                                .sorted()
-                                .collect(Collectors.toList());
-        
-        // List를 int[] 배열로 변환
-        int[] answer = temp.stream()
-                           .mapToInt(Integer::intValue)
-                           .toArray();
-        
+
+        Collections.sort(list);
+        answer = list.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
