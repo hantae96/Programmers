5e915ab7f11ce516c10d0e31506fedf7b8723635
hantae <85547680+hantae96@users.noreply.github.com>
2024-12-05 00:21:38 +0900

    [Bronze IV] Title: 숫자의 합, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,16 +1,24 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
 
-        String tmp = sc.next();
+        int n = sc.nextInt();
+        sc.nextLine();
+        String input = sc.nextLine();
+
+        char[] arrays = input.toCharArray();
+
         int answer = 0;
-        for (int i = 0;i<count;i++){
-            answer += tmp.charAt(i)-'0';
+        for(int i = 0;i<arrays.length;i++){
+            answer += arrays[i] - '0';
         }
+
         System.out.println(answer);
     }
 }
