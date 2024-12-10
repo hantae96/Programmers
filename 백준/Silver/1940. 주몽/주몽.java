ca8c5eaa4b72a7dea53f7769703ca8527fb105e4
hantae <85547680+hantae96@users.noreply.github.com>
2024-12-10 21:23:39 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16396 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,28 +1,41 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        int m = sc.nextInt();
-        sc.nextLine();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int [] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-        int answer = 0;
-        for(int i = 0;i<arr.length -1;i++){
-            for(int j = i+1;j<arr.length;j++){
-                if(arr[i] + arr[j] == m) answer++;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(array);
+
+        int start = 0, end = n-1, answer = 0;
+
+        while(start != end){
+            int sum = array[start] + array[end];
+
+            if(sum < m){
+                start++;
+            }else if(sum > m){
+                end--;
+            }else{
+                // 일치할때
+                answer++;
+                start++;
             }
         }
 
+
         System.out.println(answer);
-
-
     }
 }
