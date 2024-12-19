4c2afa8e78cd47f3422e68e190f4dc1dbf82d80c
hantae <85547680+hantae96@users.noreply.github.com>
2024-12-19 23:02:54 +0900

    [Silver IV] Title: 좋은 단어, Time: 280 ms, Memory: 22088 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -1,46 +1,42 @@
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    public static int [] index;
-    public static int [] output;
-    public static int N,R;
-
-
-    public static void main(String[] args) throws IOException {
-
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int answer = 0;
-        for(int i = 0;i<n;i++){
-            String input = sc.nextLine();
-            Stack<String> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i<n;i++){
+            String input = br.readLine();
+            Stack<Character> stack = new Stack<>();
+
             for(int j =0;j<input.length();j++){
-                String temp = String.valueOf(input.charAt(j));
+                char c = input.charAt(j);
+
                 if(stack.isEmpty()){
-                    stack.push(temp);
+                    stack.push(c);
+                    continue;
+                }
+
+                if(stack.peek() == c){
+                    stack.pop();
                 }else{
-                    String top = stack.peek();
-                    if(top.equals(temp)){
-                        stack.pop();
-                    }else{
-                        stack.push(temp);
-                    }
+                    stack.push(c);
                 }
             }
 
-            if(stack.isEmpty()){
-                answer++;
-            }
+        if(!stack.isEmpty()){
+            continue;
         }
-
-        System.out.println(answer
-        );
+        answer++;
     }
 
+    System.out.println(answer);
+
+    }
 }
