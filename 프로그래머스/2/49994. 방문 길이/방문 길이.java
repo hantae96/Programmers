0654d2c3aa5190fc7047ad4d57513a10d1075e11
hantae <85547680+hantae96@users.noreply.github.com>
2025-03-29 01:38:40 +0900

    [level 2] Title: 방문 길이, Time: 14.15 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,48 +1,77 @@
-// package org.zerock;
-
 import java.util.*;
 
 class Solution {
+    static String convert(int x, int y){
+        return x + "" + y + "";
+    }
+    
+    static boolean check(int num){
+        if(num < -5 || num > 5){
+            return false;
+        }
+        return true;
+    }
+    
     public int solution(String dirs) {
         int answer = 0;
-
-        HashSet<String> visited = new HashSet<>();
-
-        int x = 0, y = 0, nx = 0, ny = 0;
-        for(int i = 0;i<dirs.length();i++){
-            char command = dirs.charAt(i);
-            if(command == 'U'){
-                nx = x;
-                ny = y + 1;
-            }else if(command == 'D'){
-                nx = x;
-                ny = y - 1;
-            }else if(command == 'L'){
-                nx = x - 1;
-                ny = y;
-            }else if(command == 'R') {
-                nx = x + 1;
-                ny = y;
+        
+        ArrayList<String> list = new ArrayList<>();
+        
+        int x = 0;
+        int y = 0;
+        int nx = 0;
+        int ny = 0;
+        for(char c : dirs.toCharArray()){
+            boolean flag = true;
+            if(c == 'U'){
+                if(check(y+1)){
+                    ny = y+1;
+                }else{
+                    flag = false;
+                }
             }
+            if(c == 'L'){
+                if(check(x-1)){
+                    nx = x-1;
+                }else{
+                    flag = false;
+                }
+            }
+            if(c == 'R'){
+                if(check(x+1)){
+                    nx = x+1;
+                }else{
+                    flag = false;
+                }
+            }
+            if(c == 'D'){
+                if(check(y-1)){
+                    ny = y-1;
+                }else{
+                    flag = false;
+                }
+            }
+            
 
-
-            if(nx < -5 || nx > 5 || ny < -5 || ny > 5){
+            if(!flag){
                 continue;
             }
-
-            String leftPath = x + " " + y + " -> " + nx + " " + ny;
-            String rightPath = nx + " " + ny + " -> " + x + " " + y;
-
-            if(!visited.contains(leftPath) && !visited.contains(rightPath)){
+            
+            String path = convert(x,y) + convert(nx,ny);
+            String path2 = convert(nx,ny) + convert(x,y);
+            
+            
+            if(!list.contains(path) && !list.contains(path2)){
                 answer++;
             }
-
-
-            visited.add(leftPath);
-            visited.add(rightPath);
-
+            
+            list.add(path);
+            list.add(path2);
             x = nx;
-            y = ny;
+            y= ny;
         }
+        
+        
         return answer;
-    }}
+    }
+}
