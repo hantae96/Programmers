ac8809b8b8ef2756f25b26b9f3b4ef9c8765bd3f
hantae <85547680+hantae96@users.noreply.github.com>
2025-10-13 23:07:13 +0900

    [Silver V] Title: 수들의 합 5, Time: 148 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,45 +1,33 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static int[] array;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        array = new int[n+1];
-        for(int i = 1; i<n+1;i++){
-            array[i] = i;
-        }
 
-        int start = 0;
-        int end = 0;
-        int answer = 1; // n 자체를 직접 넣을 수 있음
-
-        while(end != n){
-            if(sum(start,end) > n){
-               start++;
-            } else if (sum(start,end) < n) {
-                end++;
-            }else{
-                answer++;
-                end++;
+        int leftPtr = 0;
+        int rightPtr = 1;
+        int sum = rightPtr - leftPtr;
+        int count = 0;
+        while(leftPtr <= rightPtr){
+            if(sum == n){
+                rightPtr++;
+                sum += rightPtr;
+                count++;
+            }
+            else if(sum > n){
+                sum -= leftPtr;
+                leftPtr++;
+            }
+            else{
+                rightPtr++;
+                sum += rightPtr;
             }
         }
 
-        System.out.println(answer);
-    }
+        System.out.println(count);
 
-    public static int sum(int start, int end){
-        int sum = 0;
-        for(int i = start;i<=end;i++){
-            sum += array[i];
-        }
-        return sum;
     }
 }
