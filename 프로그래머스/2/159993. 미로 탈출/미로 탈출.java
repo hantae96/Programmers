eff14e6b795ca1600d5c0723cd9b2b750fc6ab21
hantae <85547680+hantae96@users.noreply.github.com>
2025-12-01 19:37:34 +0900

    [level 2] Title: 미로 탈출, Time: 9.31 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,87 +1,92 @@
 import java.util.*;
 
-class Node {
-    int x;
-    int y;
-    int count;
-    public Node(int x, int y, int count){
-        this.x = x;
-        this.y = y;
-        this.count = count;
-    }
-}
-
 class Solution {
-    
+    static boolean[][] visited;
+    static Node lever, start, end;
+    static String[] maps;
     static int[] dx = new int[]{-1,1,0,0};
     static int[] dy = new int[]{0,0,-1,1};
-    static String[] maps;
-    static boolean[][] visited;
-    static Node start;
-    static Node end;
-    static Node lever;
+    
+    static class Node{
+        int x;
+        int y;
+        int count;
+        
+        public Node(int x,int y,int count){
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
+    }
     
     public int solution(String[] maps) {
         int answer = 0;
-        
         this.maps = maps;
-        visited = new boolean[maps.length][maps[0].length()];
         
         for(int i = 0;i<maps.length;i++){
-            for(int j = 0; j<maps[0].length();j++){
+            for(int j = 0;j<maps[i].length();j++){
                 if(maps[i].charAt(j) == 'S'){
                     start = new Node(i,j,0);
-                }else if(maps[i].charAt(j) == 'L'){
-                    lever = new Node(i,j,0);
-                }else if(maps[i].charAt(j) == 'E'){
+                }
+                
+                if(maps[i].charAt(j) == 'E'){
                     end = new Node(i,j,0);
+                }
+                
+                if(maps[i].charAt(j) == 'L'){
+                    lever = new Node(i,j,0);
                 }
             }
         }
         
-        visited = new boolean[maps.length][maps[0].length()];
-        int findLever = find(start,lever);
-        if(findLever == - 1) return -1;
+        int leverCount = findNode(start,lever);
+        if(leverCount == -1){
+            return -1;
+        }
+        int exitCount = findNode(lever,end);
+        if(exitCount == -1){
+            return -1;
+        }
         
-                visited = new boolean[maps.length][maps[0].length()];
-        int findEnd = find(lever,end);
-        if(findEnd == -1) return -1;
         
-        return findLever + findEnd;
+        return leverCount + exitCount;
     }
     
-    
-    static int find(Node start, Node target){
+    static int findNode(Node start, Node target){
+        visited = new boolean[maps.length][maps[0].length()];
+        
         LinkedList<Node> queue = new LinkedList<>();
         queue.add(start);
         visited[start.x][start.y] = true;
-    
-        boolean isLever = false;
         
         while(!queue.isEmpty()){
-            Node now = queue.pop();
+            Node now = queue.poll();
+            
             if(now.x == target.x && now.y == target.y){
                 return now.count;
             }
             
-            for(int i = 0;i<4;i++){
+            for(int i = 0; i<4;i++){
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
                 
-                if(0 > nx || nx >= maps.length || 0 > ny || ny >= maps[0].length()){
+                if(0 > nx || maps.length <= nx || 0 > ny || maps[0].length() <= ny){
                     continue;
                 }
                 
-                if(maps[nx].charAt(ny) == 'X') continue;
-                if(visited[nx][ny]) continue;
-               
+                if(maps[nx].charAt(ny) == 'X'){
+                    continue;
+                }
+                
+                if(visited[nx][ny]){
+                    continue;
+                }
                 
                 visited[nx][ny] = true;
-                queue.add(new Node(nx,ny,now.count+1));
+                queue.offer(new Node(nx,ny,now.count + 1));
             }
         }
-        
         return -1;
-        
     }
+    
 }
