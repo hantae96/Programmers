e5597c1995f1c65bdd3fcafea630988f5975c509
hantae <85547680+hantae96@users.noreply.github.com>
2025-12-12 21:50:53 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=java) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 0.03 ms
+메모리: 85.3 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 06일 21:41:34
+2025년 12월 12일 21:50:48
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -4,10 +4,20 @@ class Solution
     {
         int answer = 0;
 
+        
         while(a != b){
-            a = a/2 + a%2;
-            b = b/2 + b%2;
-            answer++;
+            answer ++;
+            if(a % 2 == 0){
+                a = a/2;
+            }else{
+                a = (a + 1) /2;
+            }
+            
+            if(b % 2 == 0){
+                b = b/2;
+            }else{
+                b = (b + 1) /2;
+            }
         }
 
         return answer;
