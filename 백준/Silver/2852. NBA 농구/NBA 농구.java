d8b8f66a9af1d47ee3af660e4348980c37e6e613
hantae <85547680+hantae96@users.noreply.github.com>
2025-12-21 07:12:33 +0900

    [Silver III] Title: NBA 농구, Time: 116 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.java
+++ 백준/Silver/2852. NBA 농구/NBA 농구.java
@@ -5,108 +5,59 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
 
-        int[] firstTeamData = new int[2]; // [스코어,합계분수]
-        int[] secondTeamData = new int[2];
+        int teamAScore = 0;
+        int teamBScore = 0;
+        int teamASecond = 0;
+        int teamBSecond = 0;
 
-        boolean add = false;
-        int winTeam = 0;
-        String startTime = "";
+        int prev = 0;
+
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+
             int teamId = Integer.parseInt(st.nextToken());
-            String time = st.nextToken();
+            int second = convertSecond(st.nextToken());
 
-            // 점수 갱신
-            if (teamId == 1) {
-                firstTeamData[0] += 1;
-            } else {
-                secondTeamData[0] += 1;
-            }
+            if (teamAScore > teamBScore)
+                teamASecond += (second - prev);
+            else if (teamAScore < teamBScore)
+                teamBSecond += (second - prev);
 
-            if (i == 0) {
-                startTime = time;
-                winTeam = teamId;
-                add = true;
-                
-            } else {
-                if (firstTeamData[0] == secondTeamData[0]) {
-                    if (winTeam == 1) {
-                        firstTeamData[1] += convertMin(time) - convertMin(startTime);
-                    } else {
-                        secondTeamData[1] += convertMin(time) - convertMin(startTime);
-                    }
-                    winTeam = 0;
-                    add = false;
-                } else if (firstTeamData[0] > secondTeamData[0]) {
-                    if (add) {
-                        firstTeamData[1] += convertMin(time) - convertMin(startTime);    
-                    }
-                    
-                    add = true;
-                    winTeam = 1;
-                    startTime = time;
-                } else if (firstTeamData[0] < secondTeamData[0]) {
-                    if (add) {
-                        secondTeamData[1] += convertMin(time) - convertMin(startTime);    
-                    }
-                    
-                    add = true;
-                    winTeam = 2;
-                    startTime = time;
-                }
-                
-            }
+            if (teamId == 1)
+                teamAScore++;
+            else
+                teamBScore++;
 
-
-            // 마지막일경우
-            if (i == n - 1) {
-                if (winTeam == 1) {
-                    firstTeamData[1] += convertMin("48:00") - convertMin(startTime);
-                } else if (winTeam == 2) {
-                    secondTeamData[1] += convertMin("48:00") - convertMin(startTime);
-                }
-            }
+            prev = second;
         }
 
-        System.out.println(convertHour(firstTeamData[1]));
-        System.out.println(convertHour(secondTeamData[1]));
+        if (teamAScore > teamBScore) {
+            teamASecond += (convertSecond("48:00") - prev);
+        } else if (teamAScore < teamBScore) {
+            teamBSecond += (convertSecond("48:00") - prev);
+        }
+
+        System.out.printf("%s\n", convertFormat(teamASecond));
+        System.out.printf("%s",convertFormat(teamBSecond));
+
+
+
     }
     
-    static String convertHour(int totalTime) {
-        String value = "";
-
-        int hour = totalTime / 60;
-        int min = totalTime % 60;
-        
-        if (hour < 10) {
-            value += "0" + hour;
-        } else {
-            value += hour;
-        }
-
-        value += ":";
-
-        if (min < 10) {
-            value += "0" + min;
-        } else {
-            value += min;
-        }
-
-        return value;
+    static String convertFormat(int second) {
+        return String.format("%02d:%02d", second / 60, second % 60);
     }
-    
- 
-    static int convertMin(String time) {
-        int totalTime = 0;
 
-        String[] temp = time.split(":");
-        // System.out.println(Arrays.toString(temp));
-        totalTime += Integer.parseInt(temp[0]) * 60;
-        totalTime += Integer.parseInt(temp[1]);
 
-        return totalTime;
+    static int convertSecond(String input) {
+        int second = 0;
+        String[] split = input.split(":");
+        second += Integer.parseInt(split[0]) * 60;
+        second += Integer.parseInt(split[1]);
 
+        return second;
     }
 }
