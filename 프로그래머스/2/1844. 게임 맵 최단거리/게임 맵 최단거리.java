bcc52f092fdd4e500caabb905d8733497633b179
hantae <85547680+hantae96@users.noreply.github.com>
2026-01-04 00:39:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.55 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,63 +1,44 @@
-import java.util.LinkedList;
+import java.util.*;
+
 
 class Solution {
-
-    static class Node {
-        int x;
-        int y;
-        int count;
-
-        public Node(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-    }
-
-    static int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
-    static int[] dy = {0, 0, -1, 1};
-    static int n, m;
+    static int[] dx = new int[]{-1,1,0,0};
+    static int[] dy = new int[]{0,0,-1,1};
     static int[][] maps;
     static boolean[][] visited;
-
     public int solution(int[][] maps) {
-        this.n = maps.length;      // 행의 개수
-        this.m = maps[0].length;   // 열의 개수
         this.maps = maps;
-        this.visited = new boolean[n][m]; // n x m 크기의 visited 배열 초기화
-
+        this.visited = new boolean[maps.length][maps[0].length];
+        
         return bfs();
     }
-
-    static int bfs() {
-        LinkedList<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 0, 1)); // 시작점 추가
+    
+    static int bfs(){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0,0,1}); // 맨처음에 1칸 세고 시작
         visited[0][0] = true;
-
-        while (!queue.isEmpty()) {
-            Node now = queue.remove();
-
-            // 도착지점에 도달했는지 확인
-            if (now.x == m - 1 && now.y == n - 1) {
-                return now.count; // 최단 경로 반환
+        
+        while(!queue.isEmpty()){
+            int[] now = queue.poll();
+            
+            if(now[0] == maps.length-1 && now[1] == maps[0].length -1){
+                return now[2];
             }
-
-            // 4방향 탐색
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-
-                // 유효한 위치인지 확인
-                if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-
-                // 방문하지 않았고, 이동 가능한 곳인지 확인
-                if (maps[ny][nx] == 1 && !visited[ny][nx]) {
-                    queue.add(new Node(nx, ny, now.count + 1)); // 새 노드 추가
-                    visited[ny][nx] = true;                    // 방문 처리
-                }
+            
+            for(int i = 0;i<4;i++){
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                
+                if(0 > nx || nx >= maps.length || 0 > ny || maps[0].length <= ny) continue;
+                if(visited[nx][ny]) continue;
+                if(maps[nx][ny] == 0) continue;
+                
+                queue.offer(new int[]{nx,ny,now[2]+1});
+                visited[nx][ny] = true;
             }
         }
-
-        return -1; // 도착할 수 없는 경우
+        
+        return -1;
+        
     }
 }
