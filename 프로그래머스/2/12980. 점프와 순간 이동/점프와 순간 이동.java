e600d6cb5cbe33cf7b83c9121aaeb01e77bd49cb
hantae <85547680+hantae96@users.noreply.github.com>
2026-01-23 20:31:56 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,24 +2,17 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
-
-        while(n > 0){
-            if(n == 1){
-                ans++;
-                n--;
-                continue;
-            }
-            
-            // 순간이동이 가능하면
-            if(n%2 == 0){
-                n = n/2;
-            }else{
-                ans++;
-                n--;
-            }
+       int ans = 0;
+    
+    while(n > 0) {
+        if(n % 2 == 1) {
+            ans++;
+            n--;
+        } else {
+            n /= 2;
         }
-
-        return ans;
+    }
+    
+    return ans;
     }
 }
